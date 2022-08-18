449b3956386f9331fbea865ab5b1d7cf4971e12f
Note Dharmapanij <parun.dharma@gmail.com>
2022-08-18 14:06:40 +0700

    js font shadows

--- font-effects01.js
+++ font-effects01.js
@@ -4,8 +4,34 @@ function selectElement(selector) {
     throw new Error(`Check "${selector}" is correct`);
 }
 
+function createShadow(element) {
+    let shadowLength = parseInt(element.dataset.shadowLength);
+    let shadowColor = element.dataset.shadowColor;
+    let textShadowContent = "";
 
 
+    [...Array(shadowLength + 1)].forEach((_, i) => {
+        textShadowContent = textShadowContent + 
+       `${i+1}px ${i+1}px 0 ${shadowColor},` ;
+    });
+
+    textShadowContent = textShadowContent.slice(0, -1);
+    return textShadowContent
+}
+
+// Apply all stretch shadows 
+let allJSShadow = document.querySelectorAll(".js-shadow");
+
+allJSShadow.forEach((element) => {
+
+    element.parentNode.addEventListener("mouseenter", ()=> {
+        element.style.textShadow = createShadow(element)
+    });
+
+    element.parentNode.addEventListener("mouseleave", ()=> {
+        element.style.transition = "all 500ms ease"
+        element.style.textShadow = ""
+    });
+});
 
 
-console.log("It Works!");
